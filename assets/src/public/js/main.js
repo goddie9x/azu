def86ad80c068c9097334fb10a1a890d99bae77f
goddie9x <goddie9x@yahoo.com>
2021-09-05 13:16:09 +0700

    full option home page

--- assets/src/public/js/main.js
+++ assets/src/public/js/main.js
@@ -22,16 +22,63 @@ $(document).ready(function() {
             720: {
                 items: 2
             },
-            540: {
+            0: {
                 items: 1
             }
         },
         loop: true,
-        height: 405,
         dots: false,
         margin: 1,
         autoplay: true,
-        autoplayTimeout: 3000,
+        autoplayTimeout: 3500,
+        smartSpeed: 500,
+        autoplayHoverPause: true
+    });
+    $('.news_container').owlCarousel({
+        items: 4,
+        responsive: {
+            1320: {
+                items: 4
+            },
+            960: {
+                items: 4
+            },
+            720: {
+                items: 2
+            },
+            0: {
+                items: 1
+            }
+        },
+        loop: true,
+        dots: false,
+        margin: 30,
+        autoplay: true,
+        autoplayTimeout: 3800,
+        smartSpeed: 500,
+        autoplayHoverPause: true
+    });
+    $('.partners_slide').owlCarousel({
+        items: 5,
+        responsive: {
+            1320: {
+                items: 5
+            },
+            960: {
+                items: 4
+            },
+            720: {
+                items: 2
+            },
+            0: {
+                items: 1
+            }
+        },
+        loop: true,
+        dots: false,
+        margin: 50,
+        autoplay: true,
+        autoplayTimeout: 3200,
         smartSpeed: 500,
         autoplayHoverPause: true
     });
